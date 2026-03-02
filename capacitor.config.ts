38fa2e2e746c85302875c4a86a59fe64b6e7fb29
gamalielherrera <gamalielherrera14@gmail.com>
2026-03-02 12:47:35 -0600

    chore: Initialize Android string resources and update Capacitor config with the new app name and ID.

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.minimalist.shinytracker',
-  appName: 'MinimalistShinyTracker',
+  appId: 'com.shinycheck.app',
+  appName: 'ShinyCheck',
   webDir: 'dist'
 };
 
